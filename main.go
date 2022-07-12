ad1a6dcf6ac985f563e603432fc553802c344802
Lars With <lars@with.de>
2022-07-12 09:03:10 +0200

    switch to sdk

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"context"
-	"terraform-provider-mailcow/mailcow"
+	"github.com/l-with/terraform-provider-mailcow/mailcow"
 
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	tfsdk.Serve(context.Background(), mailcow.New, tfsdk.ServeOpts{
-		Name: "mailcow",
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return mailcow.Provider()
+		},
 	})
 }
