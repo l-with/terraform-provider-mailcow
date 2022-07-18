8f6ea06cf3d91e6dc22364fe6fbe9e2ca4bb4410
Lars With <lars@with.de>
2022-07-18 11:28:31 +0200

    add documentation template for provider

---
+++ examples/provider/provider.tf
@@ -0,0 +1,14 @@
+resource "mailcow_domain" "demo" {
+  domain = "440044.xyz"
+}
+
+resource "mailcow_mailbox" "demo" {
+  domain     = mailcow_domain.demo.domain
+  local_part = "test"
+  password   = "initial secretpassord"
+}
+
+resource "mailcow_alias" "demo" {
+  address = "alias-demo@440044.xyz"
+  goto    = mailcow_mailbox.demo.id
+}
