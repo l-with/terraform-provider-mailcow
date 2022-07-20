1db78fa1383901129537a8f797879c3dc6c1d41c
Lars With <lars@with.de>
2022-07-20 18:12:16 +0200

    add test for mailbox

---
+++ mailcow/resource_mailbox_test.go
@@ -0,0 +1,39 @@
+package mailcow
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceMailbox(t *testing.T) {
+	domain := "domain-with.440044.xyz"
+	localPart := "mailbox-with"
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceMailboxSimple(domain, localPart),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("mailcow_mailbox.mailbox", "address", localPart+"@"+domain),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceMailboxSimple(domain string, localPart string) string {
+	return fmt.Sprintf(`
+resource "mailcow_domain" "domain" {
+  domain = "%[1]s"
+}
+
+resource "mailcow_mailbox" "mailbox" {
+  domain     = mailcow_domain.domain.domain 
+  local_part = "%[2]s"
+  password   = "password"
+  quota      = 3072
+}`, domain, localPart)
+}
