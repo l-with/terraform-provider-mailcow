b23542e5030b3a4d57bed5d0be94d2a462cc1cce
Lars With <lars@with.de>
2025-05-04 08:29:49 +0200

    add test to fail

--- mailcow/data_source_mailbox_test.go
+++ mailcow/data_source_mailbox_test.go
@@ -3,6 +3,7 @@ package mailcow
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -13,17 +14,19 @@ func TestAccDataSourceMailbox(t *testing.T) {
 	localPart := fmt.Sprintf("with-ds-mailbox-%s", randomLowerCaseString(4))
 	fullName := "full name"
 	authSource := "mailcow"
+	quota := 10240
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceMailbox(domain, localPart, fullName),
+				Config: testAccDataSourceMailbox(domain, localPart, fullName, quota),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.mailcow_mailbox.mailbox", "domain", domain),
 					resource.TestCheckResourceAttr("data.mailcow_mailbox.mailbox", "local_part", localPart),
 					resource.TestCheckResourceAttr("data.mailcow_mailbox.mailbox", "full_name", fullName),
 					resource.TestCheckResourceAttr("data.mailcow_mailbox.mailbox", "authsource", authSource),
+					resource.TestCheckResourceAttr("data.mailcow_mailbox.mailbox", "quota", strconv.Itoa(quota)),
 				),
 			},
 			{
@@ -34,7 +37,7 @@ func TestAccDataSourceMailbox(t *testing.T) {
 	})
 }
 
-func testAccDataSourceMailbox(domain string, localPart string, fullName string) string {
+func testAccDataSourceMailbox(domain string, localPart string, fullName string, quota int) string {
 	return fmt.Sprintf(`
 resource "mailcow_domain" "domain" {
   domain = "%[1]s"
@@ -45,12 +48,13 @@ resource "mailcow_mailbox" "mailbox" {
   domain     = mailcow_domain.domain.id
   password   = "secret-password"
   full_name  = "%[3]s"
+  quota      = %d
 }
 
 data "mailcow_mailbox" "mailbox" {
   address = mailcow_mailbox.mailbox.address
 }
-`, domain, localPart, fullName)
+`, domain, localPart, fullName, quota)
 }
 
 func testAccDataSourceMailboxError() string {
